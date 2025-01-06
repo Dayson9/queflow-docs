5e286cf7bcd806f5d752b12aea839705d3074ffe
dayson9 <ainsteinwells@gmail.com>
2025-01-06 21:54:57 +0100

    update

---
+++ docs/src/nuggets/Text.js
@@ -0,0 +1,20 @@
+import { Nugget } from 'queflow'
+
+const Text = new Nugget("Text", {
+  template: (data) => {
+    return `
+      <span ${data.align ? 'text-align={{ align }}' : ''} ${data.color ? 'color={{ color }}' : ''} ${data.font ? ' font-family={{ font }}' : ''} ${data.size ? ' font-size={{ size }}px' : ''} ${data.weight ? ' font-weight={{ weight }}' : ''} ${ data.bottom ? 'margin-bottom={{ bottom }}px' : '' } ${ data.class ? 'class={{ class }}' : ''}>{{ txt }}</span>
+    `
+  },
+
+  stylesheet: {
+    'span': `
+      display: block;
+      font-size: 12px;
+      text-align: center;
+      font-weight: 540;
+    `
+  }
+})
+
+export default Text
