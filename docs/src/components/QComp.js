db023accb1c1d31681de54552da8360fd874c430
dayson9 <ainsteinwells@gmail.com>
2025-01-10 09:48:42 +0100

    update

--- docs/src/components/QComp.js
+++ docs/src/components/QComp.js
@@ -13,7 +13,7 @@ const QComp = new subComponent('QComp', {
         <Paragraph { txt: 'QComponent is a class that allows to create and manage components with reactive data and dynamic rendering. It is used for rendering a QueFlow app.' } />
         
         <Paragraph { txt: 'Syntax:', top: 20 } />
-        <CodeView { codes: ['const App = new QComponent(//selector of mount node [string], {', '   data: {', '     //reactive data [object]', '   },', '    template: //HTML/JSX structure of app [string|function]'] } />
+        <CodeView { codes: ['QComponent("#app"/** selector of mount node [string] **/ {', '   data: {', '     // Reactive data [object] -&gt; An object containing reactive data, any changes made to it would automatically trigger an update in the UI.', '   },', '    template:', '      /** [string|function] -&gt; A string or a function that returns the HTML/JSX structure of app. **/ ,', '     stylesheet: {', '      // [object] An object containing CSS declarations for app.', '   },'] } />
         <Note { txt: 'The expressions in brackets are the datatypes of the corresponding properties.' } />
       </section>
     `
