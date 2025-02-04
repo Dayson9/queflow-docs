c6b733a7391879813943ae9b75d21258ac1bf034
dayson9 <ainsteinwells@gmail.com>
2025-02-04 13:17:40 +0100

    update Paragraph.js

--- public/src/nuggets/Paragraph.js
+++ public/src/nuggets/Paragraph.js
@@ -8,7 +8,7 @@ const Paragraph = new Nugget("Paragraph", {
     data.txt = data.txt.replace(hlReg, (match) => `<span class="highlighted" ${ data.font ? `font-family="${data.font}"` : '' }>${ match.slice(1, match.length-1)}</span>`)
    
     return `
-      <span ${ data.align ? 'text-align={{ align }}' : '' } class="inter" ${ data.top ? 'margin-top={{ top }}px' : '' }>
+      <span ${ data.align ? 'text-align={{ align }}' : '' } class="inter" ${ data.top ? 'margin-top="{{ top }}px"' : '' }>
         {{ txt }}
       <span>
     `
