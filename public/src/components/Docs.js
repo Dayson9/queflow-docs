3874eb0d2d849241557554049882e713583f3806
dayson9 <ainsteinwells@gmail.com>
2025-02-06 17:30:14 +0100

    update Docs.js

--- public/src/components/Docs.js
+++ public/src/components/Docs.js
@@ -7,10 +7,11 @@ const Docs = new Component('Docs', {
           <Heading { txt: "Documentation", size: 36 } />
           <Paragraph { txt: "This page contains everything you need to start using  QueFlowJS in your projects." } />
           <ListItem { items: ["#Core APIs",
-            { label: "@App [class]", click: '/docs_app' },
-            { label: "@Component [class]", click: '/docs_component' },
-            { label: "@Nugget [class]", click: '/docs_nuggets' },
-            { label: "@Template [class]" , click: '/docs_template'},
+            { label: "@App", click: '/docs_app' },
+            { label: "@Component", click: '/docs_component' },
+            { label: "@Component & App Methods" , click: '/docs_methods'},
+            { label: "@Nugget", click: '/docs_nuggets' },
+            { label: "@Template" , click: '/docs_template'},
             "#Advanced",
             { label: "@Template Syntax" , click: '/docs_template-syntax'},
             { label: "@Event Handling" , click: '/docs_events'},
