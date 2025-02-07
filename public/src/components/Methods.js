b7f7f7eefbf7b0faf5acabfac002ad14bad90c98
dayson9 <ainsteinwells@gmail.com>
2025-02-07 14:45:16 +0100

    update

--- public/src/components/Methods.js
+++ public/src/components/Methods.js
@@ -11,7 +11,7 @@ const Methods = new Component('Methods', {
         <P { txt: "There are currently 7 built-in and 4 non built-in methods, they are:", top: 20, bottom: 20 } />
         
         <SPLink { label: "[freeze()]", id: "fr" } />
-        <SPLink { label: "[unfreeze()]", id: "fr" } />
+        <SPLink { label: "[unfreeze()]", id: "unfr" } />
         <SPLink { label: "[hide()]", id: "fr" } />
         <SPLink { label: "[show()]", id: "fr" } />
         <SPLink { label: "[destroy()]", id: "fr" } />
@@ -25,17 +25,40 @@ const Methods = new Component('Methods', {
         <Heading { txt: "freeze()", size: 25, id: "fr" } />
         <P { txt: "This method is used for freezing a Component/App instance. When a Component/App instance is frozen, it stops being reactive, which means any changes made to it's data would not trigger any updates. This is useful in an application where you need to disable reactivity." } />
            <CodeView { code: \`
-// This would work 
+// Freeze Component and App
 MyComponent.freeze()
-// This would also work
 MyApp.freeze()
-
 
 
 // Updates won't trigger
 MyApp.data.title = 'App Methods'
 MyComponent.data.count++
 \` } />
+
+        <Heading { txt: "unfreeze()", size: 25, id: "unfr" } />
+        <P { txt: "As the name suggests, unfreeze() does the opposite of freeze(), it is used for unfreezing a Component/App instance." } />
+           <CodeView { code: \`
+// Freeze Component and App
+MyComponent.freeze()
+MyApp.freeze()
+
+
+// Updates won't trigger
+MyApp.data.title = 'App Methods'
+MyComponent.data.count++
+
+// Unfreeze Component and App
+MyApp.unfreeze()
+MyComponent.unfreeze()
+
+
+// Updates work now
+MyApp.data.title = 'App Methods'
+MyComponent.data.count++
+\` } />
+
+        <Heading { txt: "hide()", size: 25, id: "fr" } />
+        
         <Navigator { left: ['Components', '/docs_component'], right: ['Nuggets', '/docs_nuggets'] } />
       </section>
       `
