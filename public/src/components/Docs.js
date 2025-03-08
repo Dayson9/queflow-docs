86494429c624597401752062dcba3e72f8ef28ba
dayson9 <ainsteinwells@gmail.com>
2025-03-08 02:54:30 +0100

    update Docs.js

--- public/src/components/Docs.js
+++ public/src/components/Docs.js
@@ -6,19 +6,20 @@ const Docs = new Component('Docs', {
         <section>
           <Heading { txt: "Documentation", size: 36 } />
           <P { txt: "This page contains everything you need to start using  QueFlowJS in your projects." } />
-          <ListItem { items: ["#Core APIs",
+          <ListItem { items: ["#Introduction",
+            { label: "@Quick Start", click: '/quick-start' },
+            { label: "@Template Syntax", click: '/docs_template-syntax' }, "#Core APIs",
             { label: "@App", click: '/docs_app' },
             { label: "@Component", click: '/docs_component' },
             { label: "@Component & App Methods" , click: '/docs_methods'},
             { label: "@Nugget", click: '/docs_nuggets' },
             { label: "@Template" , click: '/docs_template'},
             "#Advanced",
-            { label: "@Template Syntax" , click: '/docs_template-syntax'},
             { label: "@Event Handling" , click: '/docs_events'},
             { label: "@Reactivity" , click: '/docs_reactivity'},
             { label: "@Global State" , click: '/docs_global_state'},
             "#Extras",
-            { label: "@HTML Highlighting" , click: '/docs_highlighting'},
+            { label: "@Syntax Highlighting" , click: '/docs_highlighting'},
             { label: "@Project Structure" , click: '/docs_project-structure'}
             ],
             top: 30 } />
